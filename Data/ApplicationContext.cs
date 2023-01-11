fadc81ebec9fa8ae7bab4eefee8d325673947b37
Weyne <augustoweyne12@gmail.com>
2023-01-10 21:12:08 -0300

    Feature: Criando indeces

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -22,20 +22,28 @@ namespace Curso.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AI");//Troca as regras de busca do banco de dados de forma global
-            //RAFAEL --> rafael -- Vai encontrar
-            //João --> Joao -- Vai encontrar
+            // modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AI");//Troca as regras de busca do banco de dados de forma global
+            // //RAFAEL --> rafael -- Vai encontrar
+            // //João --> Joao -- Vai encontrar
             
-            modelBuilder.Entity<Departamento>().Property(p => p.Descricao).UseCollation("SQL_Latin1_General_CP1_CS_AS");//Troca as regras de busca do banco de dados por entidade
+            // modelBuilder.Entity<Departamento>().Property(p => p.Descricao).UseCollation("SQL_Latin1_General_CP1_CS_AS");//Troca as regras de busca do banco de dados por entidade
 
-            modelBuilder.HasSequence<int>("MinhaSequencia", "sequencias")
-                .StartsAt(1)
-                .IncrementsBy(2)
-                .HasMin(1)
-                .HasMax(10)
-                .IsCyclic();
+            // modelBuilder.HasSequence<int>("MinhaSequencia", "sequencias")
+            //     .StartsAt(1)
+            //     .IncrementsBy(2)
+            //     .HasMin(1)
+            //     .HasMax(10)
+            //     .IsCyclic();
             
-            modelBuilder.Entity<Departamento>().Property(p => p.Id).HasDefaultValueSql("NEXT VALUE FOR sequencias.MinhaSequencia");
+            // modelBuilder.Entity<Departamento>().Property(p => p.Id).HasDefaultValueSql("NEXT VALUE FOR sequencias.MinhaSequencia");
+
+            modelBuilder
+                .Entity<Departamento>()
+                .HasIndex(p => new { p.Descricao, p.Ativo })//Indices Composto
+                .HasDatabaseName("idx_meu_indice_composto")//Dar um nome ao indice
+                .HasFilter("Descricao IS NOT NULL")//Filtrar os campos
+                .HasFillFactor(80)
+                .IsUnique();//Para criar ele unico
         }
     }
 }
